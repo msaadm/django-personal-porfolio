ab17cb0a9c389f91a47e144db129124f5a7a5ec5
Muhammad Saad Mateen <saad.5690@gmail.com>
2020-01-18 04:46:37 +0500

    Final version with home and job single page with dynamic summary and image on both pages.

--- jobs/admin.py
+++ jobs/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Job
 
-# Register your models here.
-admin.site.register(Job)
+
+@admin.register(Job)
+class JobAdmin(admin.ModelAdmin):
+    list_display = ['summary']
--- jobs/views.py
+++ jobs/views.py
@@ -1,6 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Job
+from datetime import date
 
 
 def home(request):
-    return render(request, 'jobs/home.html')
+    jobs = Job.objects.all()
+    return render(request, 'jobs/home.html', {'jobs': jobs, 'year': date.today().year})
 
+
+def show(request, job_id):
+    job = get_object_or_404(Job, pk=job_id)
+    return render(request, 'jobs/show.html', {'job': job})
